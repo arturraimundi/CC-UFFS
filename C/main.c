aa0e8f6fafc98fb2eac1e63dcac6abe4dd6723f0
arturraimundi <arturraimundi@gmail.com>
2026-03-20 12:14:34 -0300

    imagemC

---
+++ C/main.c
@@ -0,0 +1,32 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <stdio.h>
+
+int main() {
+    int w, h, c;
+
+    unsigned char *img = stbi_load("imagem.png", &w, &h, &c, 0);
+
+    if (!img) {
+        printf("Erro ao carregar imagem\n");
+        return 1;
+    }
+
+    printf("w=%d h=%d canais=%d\n", w, h, c);
+
+    unsigned char *p = img;
+
+    for (int i = 0; i < w * h; i++) {
+        unsigned char r = *p++;
+        unsigned char g = *p++;
+        unsigned char b = *p++;
+
+        printf("R=%d G=%d B=%d\n", r, g, b);
+
+        if (c == 4) p++; // pula alpha
+    }
+
+    stbi_image_free(img);
+    return 0;
+}
